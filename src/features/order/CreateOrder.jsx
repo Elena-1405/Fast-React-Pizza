43eacade3a6aab415d2be03141657c1b532c994a
Elena Petrova <helenofspb@gmail.com>
2026-02-23 18:52:48 +0300

    Styling components with Tailwind

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 //import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
+import Button from '../../ui/Button';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -48,13 +49,13 @@ function CreateOrder() {
       <Form method='POST'>
         <div>
           <label>First Name</label>
-          <input type='text' name='customer' required />
+          <input className='input' type='text' name='customer' required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type='tel' name='phone' required />
+            <input className='input' type='tel' name='phone' required />
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -62,7 +63,13 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type='text' name='address' required />
+            <input
+              className='input'
+              type='text'
+              name='address'
+              placeholder='Your address'
+              required
+            />
           </div>
         </div>
 
@@ -71,6 +78,7 @@ function CreateOrder() {
             type='checkbox'
             name='priority'
             id='priority'
+            className='h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-1'
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
@@ -81,12 +89,9 @@ function CreateOrder() {
 
         <div>
           <input type='hidden' name='cart' value={JSON.stringify(cart)} />
-          <button
-            disabled={isSubmitting}
-            className='inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed'
-          >
+          <Button disabled={isSubmitting}>
             {isSubmitting ? 'Placing order...' : 'Order now'}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
