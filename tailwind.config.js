2c6b61249e031e5ccad3139c1bdcad1ec2b004cf
Elena Petrova <helenofspb@gmail.com>
2026-02-25 18:14:33 +0300

    Components styled with Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} **/
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      pizza: ['Roboto Mono', 'monospace'],
+    },
+    extend: {
+      height: {
+        screen: '100dvh',
+      },
+    },
   },
   plugins: [],
 };
