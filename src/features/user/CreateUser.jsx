33e299a26e14f2af40f5dab5fe1232a57930c0fc
Elena Petrova <helenofspb@gmail.com>
2026-02-28 15:10:34 +0300

    userSlice connected to the necessary components

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 import Button from '../../ui/Button';
+import { useDispatch } from 'react-redux';
+import { updateName } from './userSlice';
+import { useNavigate } from 'react-router-dom';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!username) return;
+    dispatch(updateName(username));
+    navigate('/menu');
   }
 
   return (
@@ -18,13 +26,15 @@ function CreateUser() {
         type='text'
         placeholder='Your full name'
         value={username}
-        onChange={(e) => setUsername(e.target.value)}
+        onChange={(e) => {
+          setUsername(e.target.value);
+        }}
         className='input mb-8 w-72'
       />
 
       {username !== '' && (
         <div>
-          <Button type='primary' to='/order/new'>
+          <Button to='/order/new' type='primary'>
             Start ordering
           </Button>
         </div>
